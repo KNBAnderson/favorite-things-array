194648cf353738d47d4c632908c1c08b4c560e5c
Brian Hensley and Katlin Anderson <katlin@>
2019-04-01 09:57:00 -0700

    build form and create variables from answers.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+$(function() {
+  $("form").submit(function(event) {
+    var answer1 = $("#question1").val();
+    var answer2 = $("#question2").val();
+    var answer3 = $("#question3").val();
+    var answer4 = $("#question4").val();
+
+
+    event.preventDefault();
+  });
+});
